d62411ab4fefabfe4dac0cb8e690b0e0bf547979
mamanwhide <abdulvivo52@gmail.com>
2025-05-26 15:05:52 +0000

    update

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull
+rm -rf .cache build
+docker run -u $(id -u) -v $PWD:/antora:Z --rm -t antora/antora --cache-dir=./.cache --stacktrace antora-playbook.yml
+rm -rf /var/www/html/build
+cp -r build /var/www/html/
+sudo nginx -t
+sudo systemctl restart nginx
+rm -rf build .cache
